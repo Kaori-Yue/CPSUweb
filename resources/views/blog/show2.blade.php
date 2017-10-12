eea1fabd1732a681aec4b08f0a3722121bac07b9
Deklnwzaza <dietyboy@hotmail.com>
2017-10-12 16:45:35 +0700

    update sharetoFB and TW 2

--- resources/views/blog/show2.blade.php
+++ resources/views/blog/show2.blade.php
@@ -60,7 +60,7 @@
     </div>
 @endsection
 
-@section('script')
+
 
     <script>
         document.getElementById('shareToFB{{$blog->id}}').onclick = function() {
@@ -77,4 +77,3 @@
             window.open('http://twitter.com/share?url=' + encodeURIComponent(url) + '&text=' + encodeURIComponent(text), '', 'left=0,top=0,width=550,height=450,personalbar=0,toolbar=0,scrollbars=0,resizable=0');
         };
     </script>
-@endsection
