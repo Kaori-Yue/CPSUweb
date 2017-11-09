908af371c1d354942c45a655c43167c2f649bec8
Deklnwzaza <dietyboy@hotmail.com>
2017-11-09 22:53:17 +0700

    fix share

--- resources/views/blog/_card3.blade.php
+++ resources/views/blog/_card3.blade.php
@@ -1,25 +1,25 @@
 <div class="card-news background-light-gray">
-    <a href="{{ url('blog/'.$blog->slug) }}" style="text-decoration: none">
+    <a href="{{ url('blog/'.$blog_r->slug) }}" style="text-decoration: none">
         <ul class="container-card-news">
             <li class="image-card-news">
-                <img src="{{ url('image/show/'.$blog->cover) }}" />
+                <img src="{{ url('image/show/'.$blog_r->cover) }}" />
                 <!--<img src="23.jpeg"/>-->
             </li>
             <li class="content-card-news">
-                <p class="time-create-news">{{ $blog->publish_at }}</p>
-                <h1 class="title">{!! $blog->title !!}</h1>
-                <p class="description-card-news">{!! $blog->description !!}</p>
+                <p class="time-create-news">{{ $blog_r->publish_at }}</p>
+                <h1 class="title">{!! $blog_r->title !!}</h1>
+                <p class="description-card-news">{!! $blog_r->description !!}</p>
             </li>
         </ul>
     </a>
     <div class="detail-card-news">
-        <p class="create-by-card-news">โดย : {!! $blog->user->name !!}</p>
+        <p class="create-by-card-news">โดย : {!! $blog_r->user->name !!}</p>
         <ul class="logos-share-card-news">
-            <li class="logo-share-card-news" id="shareToFB{{$blog->id}}">
+            <li class="logo-share-card-news" id="shareToFB{{$blog_r->id}}">
                 <img src="{{ URL::asset('image/facebook.png') }}">
                 <p class="description-logo-share-card-news">Share Facebook</p>
             </li>
-            <li class="logo-share-card-news" id="shareToTW{{$blog->id}}">
+            <li class="logo-share-card-news" id="shareToTW{{$blog_r->id}}">
                 <img src="{{ URL::asset('image/twitter.png') }}">
                 <p class="description-logo-share-card-news">Share Twitter</p>
             </li>
@@ -28,17 +28,17 @@
 </div>
 
 <script>
-    document.getElementById('shareToFB{{$blog->id}}').onclick = function() {
+    document.getElementById('shareToFB{{$blog_r->id}}').onclick = function() {
         FB.ui({
             method: 'share',
             display: 'popup',
-            href: '{{ 'http://202.28.72.71/blog/'.$blog->slug }}',
+            href: '{{ 'http://202.28.72.71/blog/'.$blog_r->slug }}',
         }, function(response){});
     };
 
     document.getElementById('shareToTW{{$blog->id}}').onclick = function() {
-        var url = "{{ 'http://202.28.72.71/blog/'.$blog->slug }}";
-        var text = "{{ $blog->title }}";
+        var url = "{{ 'http://202.28.72.71/blog/'.$blog_r->slug }}";
+        var text = "{{ $blog_r->title }}";
         window.open('http://twitter.com/share?url=' + encodeURIComponent(url) + '&text=' + encodeURIComponent(text), '', 'left=0,top=0,width=550,height=450,personalbar=0,toolbar=0,scrollbars=0,resizable=0');
     };
 </script>
