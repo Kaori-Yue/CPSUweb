ac5f9f818192b84a1997ba074fcf7a4ea888ffd4
Thanadej8 <authanadej@gmail.com>
2017-12-24 00:10:38 +0700

    Add
    add navegation at detail teacher,staff

--- resources/views/teacher/detail.blade.php
+++ resources/views/teacher/detail.blade.php
@@ -3,6 +3,25 @@
 @section('title', "บุคลากรสายวิชาการ")
 @section('content')
     <div class="container-content">
+        <ul class="wrapper-nav">
+            <li class="list-nav">
+                <a href="{{url("")}}">
+                    <img src="{{ URL::asset('image/home-nav.svg') }}" class="image-home-list-nav">
+                </a>
+            </li>
+            <li class="list-nav">
+                <img src="{{ URL::asset('image/chevron-right-green.svg') }}" class="image-chevron-right-list-nav">
+            </li>
+            <li class="list-nav"><a href="{{url("person")}}">บุคลากร</a></li>
+            <li class="list-nav">
+                <img src="{{ URL::asset('image/chevron-right-green.svg') }}" class="image-chevron-right-list-nav">
+            </li>
+            <li class="list-nav"><a href="{{url("teacher")}}">บุคลากรสายวิชาการ</a></li>
+            <li class="list-nav">
+                <img src="{{ URL::asset('image/chevron-right-green.svg') }}" class="image-chevron-right-list-nav">
+            </li>
+            <li class="list-nav">$teacher_read->name_th</li>
+        </ul>
         <div class="wrapper-header-title">
             <h1 class="header-title">บุคลากรสายวิชาการ</h1>
             <div class="wrapper-buttom-next-previous">
